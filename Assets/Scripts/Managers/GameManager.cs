50dbdd17dbfc379dc0c2b9f7e3547b32fed31425
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2021-01-23 16:34:11 +0300

    Add initialization of game manager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,9 +1,42 @@
 using UnityEngine;
+using Utils;
 
 namespace Managers
 {
-    public class GameManager : MonoBehaviour
+    public class GameManager : MonoBehaviour, IInitilizable, IUnInitializeble
     {
+        // static instance of GameManager which allows it to be accessed by any other script
+        public static GameManager Instance;
 
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                if (Instance != this)
+                {
+                    // this enforces our singleton pattern, meaning there can only ever be one instance of a GameManager
+                    Destroy(gameObject);
+                }
+            }
+
+            Initialize();
+        }
+        private void OnDestroy()
+        {
+            UnInitialize();
+        }
+
+        public void Initialize()
+        {
+
+        }
+        public void UnInitialize()
+        {
+
+        }
     }
 }
