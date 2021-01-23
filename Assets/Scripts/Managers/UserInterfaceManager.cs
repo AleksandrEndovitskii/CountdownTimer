dff8485d1fb7216904803f95bc0aca711e482b6c
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2021-01-23 20:31:48 +0300

    Add dynamic instantiating of buttons window

--- Assets/Scripts/Managers/UserInterfaceManager.cs
+++ Assets/Scripts/Managers/UserInterfaceManager.cs
@@ -5,9 +5,19 @@ namespace Managers
 {
     public class UserInterfaceManager : MonoBehaviour, IInitilizable, IUnInitializeble
     {
+        [SerializeField]
+        private Canvas _userInterfaceCanvasPrefab;
+        [SerializeField]
+        private RectTransform _buttonsWindowsPrefab;
+
+        private Canvas _userInterfaceCanvasInstance;
+        private RectTransform _buttonsWindowsInstance;
+
         public void Initialize()
         {
+            _userInterfaceCanvasInstance = Instantiate(_userInterfaceCanvasPrefab);
 
+            _buttonsWindowsInstance = Instantiate(_buttonsWindowsPrefab, _userInterfaceCanvasInstance.gameObject.transform);
         }
 
         public void UnInitialize()
