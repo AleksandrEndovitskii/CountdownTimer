39b8144a878c3b218d13cc9c20ee885086193278
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2021-01-23 23:09:58 +0300

    Fix showing buttons animation

---
+++ Assets/Scripts/Components/ButtonsShowingAnimationComponent.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Components
+{
+    public class ButtonsShowingAnimationComponent : MonoBehaviour
+    {
+        [SerializeField]
+        private int _stepBetweenAnimationsPlayingSecondsCount;
+        [SerializeField]
+        private List<Button> _buttons = new List<Button>();
+
+        private void Awake()
+        {
+            _buttons = this.gameObject.GetComponentsInChildren<Button>().ToList();
+        }
+        private void Start()
+        {
+            StartCoroutine(PerformActionAfterDelayFramesCountCoroutine(1, () =>
+            {
+                for (var i = 0; i < _buttons.Count; i++)
+                {
+                    var rectTransform = _buttons[i].GetComponent<RectTransform>();
+
+                    var initialAnchoredPositionX = rectTransform.anchoredPosition.x;
+
+                    var layoutElement = rectTransform.gameObject.GetComponent<LayoutElement>();
+                    rectTransform.anchoredPosition = new Vector2(0 - layoutElement.preferredWidth,
+                        rectTransform.anchoredPosition.y);
+
+                    StartCoroutine(PerformActionAfterDelaySecondsCountCoroutine(
+                        i * _stepBetweenAnimationsPlayingSecondsCount,
+                        () =>
+                        {
+                            rectTransform.DOLocalMoveX(
+                                initialAnchoredPositionX, _stepBetweenAnimationsPlayingSecondsCount);
+                        }));
+                }
+            }));
+        }
+
+        private IEnumerator PerformActionAfterDelaySecondsCountCoroutine(int delaySecondsCount, Action action)
+        {
+            yield return new WaitForSeconds(delaySecondsCount);
+
+            action?.Invoke();
+        }
+
+        private IEnumerator PerformActionAfterDelayFramesCountCoroutine(int delayFramesCount, Action action)
+        {
+            yield return new WaitForFrames(delayFramesCount);
+
+            action?.Invoke();
+        }
+    }
+}
