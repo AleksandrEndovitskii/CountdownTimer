e36248e66594c4ac248c4238af508a7e8411f0bc
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2021-01-25 02:22:30 +0300

    Add playing of timer buttons animations on is timer stopped condition

---
+++ Assets/Scripts/Components/IsTimerStartedChangedAnimationPlayingComponent.cs
@@ -0,0 +1,36 @@
+using DG.Tweening;
+using UnityEngine;
+using Views;
+
+namespace Components
+{
+    [RequireComponent(typeof(DOTweenAnimation))]
+    public class IsTimerStartedChangedAnimationPlayingComponent : MonoBehaviour
+    {
+        [SerializeField]
+        private TimerButtonView _timerButtonView;
+
+        private DOTweenAnimation _doTweenAnimation;
+
+        private void Awake()
+        {
+            _doTweenAnimation = this.gameObject.GetComponent<DOTweenAnimation>();
+        }
+        private void Start()
+        {
+            _timerButtonView.TimerModel.IsStartedChanged += TimerModelOnIsStartedChanged;
+        }
+        private void OnDestroy()
+        {
+            _timerButtonView.TimerModel.IsStartedChanged -= TimerModelOnIsStartedChanged;
+        }
+
+        private void TimerModelOnIsStartedChanged(bool isStarted)
+        {
+            if (!isStarted)
+            {
+                _doTweenAnimation.DORestart();
+            }
+        }
+    }
+}
