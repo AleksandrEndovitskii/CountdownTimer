a96997c8b245249078ce4e866aac48ded5ea62b2
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2021-01-25 00:56:02 +0300

    Add hiding animation to timer window

---
+++ Assets/Scripts/Components/DestroyGameObjectComponent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Components
+{
+    public class DestroyGameObjectComponent : MonoBehaviour
+    {
+        public void Destroy()
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
